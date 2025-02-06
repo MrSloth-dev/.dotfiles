ca1e4c34786895598da99b5e8dce55c28970e867
joao-pol <joao-pol@student.42porto.com>
2025-02-06 16:04:13 +0000

    update

--- scripts/gitup/gitupdate.sh
+++ scripts/gitup/gitupdate.sh
@@ -4,29 +4,31 @@ set -e
 
 REPO_FILE="gitupdate.txt"
 LOG_DIR="logs"
-LOG_FILE="${LOG_DIR}"/repo_updates_$(date +%Y%m%d_%H%M%S).log
-ERROR_LOG="${LOG_DIR}"/errors_$(date +%Y%m%d_%H%M%S).log
+LOG_FILE="${LOG_DIR}"/repo_updates.log
+ERROR_LOG="${LOG_DIR}"/error.log
 
 setup_loggin() {
-    mkir -p "${LOG_DIR}"   
-    touch "${LOG_FILE}"
-    touch "${ERROR_LOG}"
+    mkdir -p ${LOG_DIR}
+    touch ${LOG_FILE}
+    touch ${ERROR_LOG}
 }
 
+setup_loggin
+
 log_info() {
-    local message="[INFO] $(date %Y%m%d_%H%M%S) - $1"
-    echo "$message" | tee -a ${LOG_FILE}
+    local message="[INFO] $(date '+%Y-%m-%d-%H:%M:%S' ) - $1"
+    echo "$message" >> ${LOG_FILE}
 }
 
 log_error() {
-    local message="[ERROR] $(date %Y%m%d_%H%M%S) - $1"
-    echo "${message}" | tee -a ${ERROR_LOG}
+    local message="[ERROR] $(date '+%Y-%m-%d-%H:%M:%S' ) - $1"
+    echo "${message}" >> -a ${ERROR_LOG}
     echo "${message}" >> ${LOG_FILE}
 }
 
 log_warning() {
-    local message="[WARNING] $(date %Y%m%d_%H%M%S) - $1"
-    echo "${message}" | tee -a ${LOG_FILE}
+    local message="[WARNING] $(date '+%Y-%m-%d-%H:%M:%S' ) - $1"
+    echo "${message}" >> ${LOG_FILE}
 }
 
 execute_cmd() {
@@ -57,41 +59,40 @@ fi
 
 
 update_repo() {
-    local REPO_PATH="$1"
+    local REPO_PATH="${1/#\~/$HOME}"
     local REPO_NAME=$(basename "${REPO_PATH}")
     log_info "Starting Process for repository: "${REPO_NAME};
     output=$(git status)
     if [[ "${output}" =~ "fatal"  ]]; then
-        log_error 
-        echo -e "$REPO_PATH is not a repository";
+        log_error "$REPO_PATH is not a repository" ;
         return 1;
     fi
     if [[ ! -d "${REPO_PATH}"  ]]; then
-        echo -e "No Repo Path found";
+        log_error "No Repo Path found";
     fi
 
-    cd "$REPO_PATH" || return 1
+    cd $REPO_PATH
+    if [[ ! $? ]]; then
+        return 1
+    fi
+
     git pull
     if [[ -n $(git status) ]]; then
-        echo -e "Changes found, add and commit everything";
+        log_info "Changes found, add and commit everything";
         git add -A
         git commit -m "Automatic Update"
         git push
-        echo -e "Repository ${REPO_PATH} updated"
+        log_info "Repository ${REPO_PATH} updated"
     else
-        echo -e "Changes not found, skipping to the next";
+        log_warning "Changes not found, skipping to the next";
     fi
-
     cd - >/dev/null/
 }
 
-
-echo -e "Starting repository update"
+log_info "Starting repository update"
 while IFS= read -r repo_path; do
     [[ -z "${repo_path}" || "${repo_path}" =~ ^#.*$ ]] && continue;
     update_repo "${repo_path}"
-    echo -e "---------------------------"
+    log_info "---------------------------"
 done < "${REPO_FILE}"
-
-
-echo -e "All repositories updated"
+log_info "All repositories updated"
