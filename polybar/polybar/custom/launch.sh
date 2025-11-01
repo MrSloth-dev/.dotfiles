84f66008caae77b8fbbd26b1835f940b2375f3b3
MrSloth-dev <joao.a.barbosa@proton.me>
2025-11-01 01:15:49 +0000

    notion and polybar

---
+++ polybar/polybar/custom/launch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch the bar
+polybar -q main -c "$HOME/.config/polybar/custom/config.ini" &
+polybar -q secondary -c "$HOME/.config/polybar/custom/config.ini" &
---
+++ scripts/launch_notion.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+firefox -new-window https://notion.so -P Notion -no-remote --kiosk
