2391adb033921bebe77d63a1684e1296e49ed28c
MrSloth-dev <joao.a.barbosa@proton.me>
2025-12-17 00:00:16 +0000

    update

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -5,7 +5,7 @@ return {
 		opts = {
 			size = 15,
 			open_mapping = [[<leader>t]],
-			direction = "horizontal",
+			direction = "float",
 			shade_terminals = false, -- Disable shading to use theme colors
 			persist_size = true,
 			close_on_exit = true,
